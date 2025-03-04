a39879d1a612fb809202a2e1dc6d2c0c4dbc9a51
nafisreza <bigganinafis@gmail.com>
2025-03-04 23:55:21 +0600

    one method has been modified.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,17 +1,7 @@
 const UserModel = require("../models/userModel");
 const { generateToken } = require("../utils/jwt");
 
-class AuthController {
-    static async register(req, res) {
-        try {
-            const user = await UserModel.create(req.body);
-            res.statusCode = 201;
-            res.end(JSON.stringify(user));
-        } catch (error) {
-            res.statusCode = 400;
-            res.end(JSON.stringify({ error: error.message }));
-        }
-    }
+
 
     static async login(req, res) {
         try {
