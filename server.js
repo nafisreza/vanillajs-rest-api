74229b1bb329a12e2a8b8b2ee13e9ef49e4b07d0
nafisreza <bigganinafis@gmail.com>
2025-03-06 06:56:50 +0600

    changes in server in the if else

--- server.js
+++ server.js
@@ -9,6 +9,24 @@ const PORT = 3000;
 const server =http.createServer((req, res) => {
     res.setHeader('Content-Type', 'application/json');
 
+    // Parse URL and query parameters
+    const parsedUrl = url.parse(req.url, true);
+    req.query = parsedUrl.query;
+    req.path = parsedUrl.pathname;
+
+    // Collect request body
+    let body = '';
+    req.on('data', chunk => {
+        body += chunk.toString();
+    });
+
+    req.on('end', () => {
+        if (body) {
+            req.body = JSON.parse(body);
+        }
+        handleRoutes(req, res);
+    });
+
 });
 
 server.listen(PORT, () => {
