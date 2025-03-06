a776579e114e48af574c2e51ffd6e57ec2790bda
nafisreza <bigganinafis@gmail.com>
2025-03-06 09:43:57 +0600

    new line added

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
+// Import the 'http' module to create an HTTP server
 const http = require('http');
 
+// Import the 'url' module to parse URLs and query parameters
 const url = require('url');
 
+// Import the 'handleRoutes' function from the routes module
 const { handleRoutes } = require('./routes/index');
 
+// Defining the port on which the server will listen
 const PORT = 3000;
 
+// Create an HTTP server
 const server =http.createServer((req, res) => {
 
     res.setHeader('Content-Type', 'application/json');
