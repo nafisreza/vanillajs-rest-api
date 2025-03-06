f20c3f0d4a076ff57478c7ce4991d23bb9166849
nafisreza <bigganinafis@gmail.com>
2025-03-06 08:41:30 +0600

    changed in the create server

--- server.js
+++ server.js
@@ -7,28 +7,43 @@ const { handleRoutes } = require('./routes/index');
 const PORT = 3000;
 
 const server =http.createServer((req, res) => {
+
     res.setHeader('Content-Type', 'application/json');
 
     // Parse URL and query parameters
+
     const parsedUrl = url.parse(req.url, true);
+
     req.query = parsedUrl.query;
+    
     req.path = parsedUrl.pathname;
 
     // Collect request body
+
     let body = '';
+    
     req.on('data', chunk => {
+    
         body += chunk.toString();
+    
     });
 
     req.on('end', () => {
+    
         if (body) {
+    
             req.body = JSON.parse(body);
+    
         }
+    
         handleRoutes(req, res);
+    
     });
 
 });
 
 server.listen(PORT, () => {
+    
     console.log(`Server running on port ${PORT}`);
-})
+
+});
