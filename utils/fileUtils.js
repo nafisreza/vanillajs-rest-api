d7f6536ed8d20e0ce535c42386a741e677e7f338
nafisreza <bigganinafis@gmail.com>
2025-03-07 11:19:49 +0600

    changed in try block

--- utils/fileUtils.js
+++ utils/fileUtils.js
@@ -4,12 +4,15 @@ const fs = require('fs').promises;
 // Import the 'path' module to handle and transform file paths
 const path = require('path');
 
+// Function to read and parse a JSON file asynchronously
 const readJsonFile = async (filename) => {
 
     try {
     
+        // Read the file asynchronously from the 'data' directory
         const data = await fs.readFile(
     
+            // Construct the file path using '__dirname' (current directory) and 'filename'
             path.join(__dirname, '..', 'data', filename) 
     
         );
