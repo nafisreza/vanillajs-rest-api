5fad5ac714eff8a0658498d732e464ff83cfedaf
Khandaker Ali Ariyan <khandakeraliariyanndc@gmail.com>
2025-03-13 18:31:21 +0600

    changed in HTTP server

--- server.js
+++ server.js
@@ -13,39 +13,30 @@ const PORT = 3000
 // Create an HTTP server
 const server = http.createServer((req, res) => {
 
-    // Setting the response header to indicate that the content type will be JSON
     res.setHeader('Content-Type', 'application/json');
 
-    // Parsing URL and query parameters
     const parsedUrl = url.parse(req.url, true);
 
-    // Attaching the query parameters to the request object
     req.query = parsedUrl.query;
     
-    // Attaching the path (URL pathname) to the request object
     req.path = parsedUrl.pathname;
 
-    // Collect the request body data
     let body = '';
     
-    // Listen for 'data' events, which are emitted when a chunk of the request body is received
     req.on('data', chunk => {
 
-        // Append each chunk of data to the 'body' variable
         body += chunk.toString();
     
     });
-
-    // Listen for the end event, which is emitted when the entire request body has been received
+    
     req.on('end', () => {
-        // If there is a body, parse it as JSON and attach it to the request object
+        
         if (body) {
     
             req.body = JSON.parse(body);
     
         }
 
-        // Call the 'handleRoutes' function to handle the request based on the route
         handleRoutes(req, res);
     
     });
@@ -53,9 +44,9 @@ const server = http.createServer((req, res) => {
 });
 
 // Start the server and make it listen on the specified port
+
 server.listen(PORT, () => {
     
-    // Log a message to the console indicating that the server is running
     console.log(`Server running on port ${PORT}`);
 
 });
