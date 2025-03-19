a4d8ae2c67257bd45f4c6e3bf5fd05087c200542
nafisreza <bigganinafis@gmail.com>
2025-03-19 12:22:32 +0600

    fix: product category case insensitivity

--- models/productModel.js
+++ models/productModel.js
@@ -1,75 +1,65 @@
-const { readJsonFile, writeJsonFile } = require('../utils/fileUtils');
+const { readJsonFile, writeJsonFile } = require("../utils/fileUtils");
 
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 
 class ProductModel {
-    
-    static async getAll(category = null) {
+  static async getAll(category = null) {
+    const products = await readJsonFile("products.json");
 
-        const products = await readJsonFile('products.json');
-        
-        if (category) {
-        
-            return products.filter(p => p.category === category);
-        
-        }
-        
-        return products;
-    
+    if (category) {
+      return products.filter(
+        (p) => p.category.toLowerCase() === category.toLowerCase()
+      );
     }
 
-    static async getById(id) {
-    
-        const products = await readJsonFile('products.json');
-    
-        return products.find(p => p.id === id);
-    
+    return products;
+  }
+
+  static async getById(id) {
+    const products = await readJsonFile("products.json");
+
+    return products.find((p) => p.id === id);
+  }
+
+  static async create(productData) {
+    const products = await readJsonFile("products.json");
+
+    const newProduct = {
+      id: uuidv4(),
+      ...productData,
+      createdAt: new Date().toISOString(),
+    };
+
+    products.push(newProduct);
+
+    await writeJsonFile("products.json", products);
+
+    return newProduct;
+  }
+
+  static async update(id, updateData) {
+    const products = await readJsonFile("products.json");
+
+    const index = products.findIndex((p) => p.id === id);
+
+    if (index === -1) {
+      return null;
     }
 
-    static async create(productData) {
-    
-        const products = await readJsonFile('products.json');
-    
-        const newProduct = {id: uuidv4(),...productData,createdAt: new Date().toISOString()};
+    products[index] = { ...products[index], ...updateData };
 
-        products.push(newProduct);
-        
-        await writeJsonFile('products.json', products);
-        
-        return newProduct;
-    
-    }
+    await writeJsonFile("products.json", products);
 
-    static async update(id, updateData) {
-     
-        const products = await readJsonFile('products.json');
-     
-        const index = products.findIndex(p => p.id === id);
-     
-        if (index === -1) {
+    return products[index];
+  }
 
-            return null;
-        
-        }
-        
-        products[index] = { ...products[index], ...updateData };
-        
-        await writeJsonFile('products.json', products);
-        
-        return products[index];
-    
-    }
+  static async delete(id) {
+    const products = await readJsonFile("products.json");
 
-    static async delete(id) {
-    
-        const products = await readJsonFile('products.json');
-    
-        const filteredProducts = products.filter(p => p.id !== id);
-    
-        await writeJsonFile('products.json', filteredProducts);
-    
-    }
+    const filteredProducts = products.filter((p) => p.id !== id);
 
+    await writeJsonFile("products.json", filteredProducts);
+  }
 }
 
-module.exports = ProductModel; 
+module.exports = ProductModel;
